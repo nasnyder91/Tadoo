cf53eb96f73f4739f67a8e61e5ca6452272cdc41
Nick Snyder <n.a.snyder@comcast.net>
2018-04-18 22:25:29 -0400

    Todo model Todo views

--- routes/todos.js
+++ routes/todos.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+// Show todos index Route
 router.get('/', (req, res) => {
   res.render('todos/index');
 });
+
+// Add Todo Route
+router.get('/add', (req, res) => {
+  res.render('todos/add');
+});
+
 
 module.exports = router;
